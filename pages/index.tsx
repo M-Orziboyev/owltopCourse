5c55c6faa444925d58cc4e5ef00b23fd03758f26
M-Orziboyev <orziboyevmuzaffar@gmail.com>
2023-06-25 17:25:07 +0500

    REMOVE asPath

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,15 +9,6 @@ import LoaderIcon from "./loader.svg"
 
 function Home(): JSX.Element {
   const [isLoading, setIsLoading] = useState<boolean>(false)
-  const router = useRouter()
-
-  useEffect(() => {
-    setIsLoading(true)
-    router.push("http://localhost:3000/courses/financial-analytics")
-    setIsLoading(false)
-    // eslint-disable-next-line
-  }, [])
-  
   return (
     <>
       {!isLoading && <div style={{height: "100vh", display: "flex", justifyContent: "center", alignItems: "center"}}><LoaderIcon /></div>}
